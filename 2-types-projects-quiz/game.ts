22f481e4a9a4ff71133ac4d17da7b1fe4ffd1541
minbr0ther <minbr0ther@naver.com>
2022-01-06 10:44:53 +0900

    3.5 좌표 게임 💡 & 3.7 로딩 상태 표시 💡

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,29 @@
 /**
  * Let's make a game 🕹
  */
+type Type = 'up' | 'down' | 'left' | 'right';
+
+const position = { x: 0, y: 0 };
+
+const move = (direction: Type) => {
+  switch (direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+    default:
+      throw Error(`unknown direction: ${direction}`);
+  }
+};
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,6 +20,22 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  const printLoginState = (loadState: ResourceLoadState) => {
+    switch (loadState.state) {
+      case 'loading':
+        console.log(`👀 ${loadState.state}...`);
+        break;
+      case 'success':
+        console.log(`😀 ${loadState.response.body}`);
+        break;
+      case 'fail':
+        console.log(`😱 ${loadState.reason}`);
+        break;
+      default:
+        throw Error(`unknown state: ${loadState}`);
+    }
+  };
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
