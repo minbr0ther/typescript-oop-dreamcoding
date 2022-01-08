678f0d4386234dc92ce7b47c2b02b9f079bf9112
minbr0ther <minbr0ther@naver.com>
2022-01-08 18:48:19 +0900

    4.7 객체지향적으로 커피기계 만들기 💡 (static 사용)

---
+++ 3-OOP/3-2-class.ts
@@ -0,0 +1,37 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    static BEANS_GRAM_PER_SHOT: number = 7; // class level
+    coffeeBeans: number = 0; // instance (object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    // 외부에서도 클래스를 만들지 않고 사용 가능
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = new CoffeeMaker(32);
+  console.log(maker); // CoffeeMaker { BEANS_GRAM_PER_SHOT: 7, coffeeBeans: 32 }
+  const maker2 = new CoffeeMaker(32);
+  console.log(maker2);
+}
