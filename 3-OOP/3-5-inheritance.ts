9536c3fb70a2022bb11cf340b58a9a25e1a2026e
minbr0ther <minbr0ther@naver.com>
2022-01-10 13:52:19 +0900

    4.13 Inheritance 상속으로 다양한 커피 기계 만들기💡

---
+++ 3-OOP/3-5-inheritance.ts
@@ -0,0 +1,91 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT: number = 7; // private 외부에서 변경 못함
+    private coffeeBeans: number = 0; // instance (object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    // 외부에서도 클래스를 만들지 않고 사용 가능
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      // 외부에서 내부의 private를 간접적으로 조작할 수 있다
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than zero');
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log('cleaning the machine... ✨');
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
+    }
+
+    private preHeat() {
+      console.log('heating up... 🔥');
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots ... ☕️`);
+
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preHeat();
+
+      return this.extract(shots);
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    // 자식클래스에서 다시 생성자 생성
+    constructor(beans: number, public readonly serialNumber: string) {
+      // 부모생성자에는 coffeeBeans:number 를 전달해야함
+      super(beans);
+    }
+
+    private steamMilk(): void {
+      console.log('Steaming some milk... 🥛');
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots); // 부모의 가열 및 등등 메소드
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  const machine = new CoffeeMachine(23);
+  const latteMachine = new CaffeLatteMachine(23, 'SSSS');
+  const coffee = latteMachine.makeCoffee(1);
+  //   console.log(coffee);
+  console.log(latteMachine.serialNumber); // 부모에서는 접근 못하는 serialNumber에 접근 가능
+}
