e93aecaaa3fb4eff8e13592b3ef845230119fb8f
minbr0ther <minbr0ther@naver.com>
2022-01-18 17:06:19 +0900

    9.5 막강한 Error State 👍

--- 8-exception/8-3-state.ts
+++ 8-exception/8-3-state.ts
@@ -3,19 +3,10 @@
 
   class OfflineError extends Error {}
 
-  type NetworkErrorState = {
-    result: 'fail';
-    reason: 'offline' | 'down' | 'timeout';
-  };
-  type SuccessState = {
-    result: 'success';
-  };
-
-  // 예상할 수 있는 성공과 실패 상태를 타입으로 정의하는 것이
-  type ResultState = SuccessState | NetworkErrorState;
-
   class NetworkClient {
-    tryconnect(): ResultState {}
+    tryconnect(): void {
+      throw new OfflineError('no network');
+    }
   }
 
   class UserService {
