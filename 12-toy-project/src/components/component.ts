a54975516ba0cd4dda3e706f34584370e5397ff3
minbr0ther <minbr0ther@naver.com>
2022-02-03 15:40:23 +0900

    14.10 컴포넌트 리팩토링💡 (베이스 컴포넌트 만들기)

---
+++ 12-toy-project/src/components/component.ts
@@ -0,0 +1,20 @@
+// 이렇게 interface를 규격해두고 사용하는 곳 에서는 component 타입만 사용하면 된다
+export interface Component {
+  // position은 optional, return은 없어서 void
+  attachTo(parent: HTMLElement, position?: InsertPosition): void;
+}
+/**
+ * HTML element 생성을 캡슐화 한다
+ */
+export class BaseComponent<T extends HTMLElement> implements Component {
+  protected readonly element: T;
+  constructor(htmlString: string) {
+    const template = document.createElement("template");
+    template.innerHTML = htmlString;
+    this.element = template.content.firstElementChild! as T;
+  }
+
+  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
+    parent.insertAdjacentElement(position, this.element);
+  }
+}
--- 12-toy-project/src/components/page/item/image.ts
+++ 12-toy-project/src/components/page/item/image.ts
@@ -1,30 +1,27 @@
-export class ImageComponent {
-  private element: HTMLElement;
+import { BaseComponent } from "../../component.js";
+
+export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    const template = document.createElement("template");
-    template.innerHTML = /*html*/ `
-    <section class="image">
-        <div class="image__holder">
-            <img class="image__thumbnail">
-        </div>
-        <p class="image__title"></p>
-    </section>`;
+    super(
+      `<section class="image">
+          <div class="image__holder">
+              <img class="image__thumbnail">
+          </div>
+          <p class="image__title"></p>
+      </section>`
+    );
 
-    this.element = template.content.firstElementChild! as HTMLElement;
-
+    // image__thumbnail를 불러와서 url, title 속성 부여
     const imageElement = this.element.querySelector(
       ".image__thumbnail"
     )! as HTMLImageElement;
     imageElement.src = url;
     imageElement.alt = title;
 
+    // image__title를 불러와서 title 부여
     const titleElement = this.element.querySelector(
       ".image__title"
     )! as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
