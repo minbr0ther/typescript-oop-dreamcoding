59b7877c94856f5ef05e815e3caffd7b4a1b03c4
minbr0ther <minbr0ther@naver.com>
2022-02-03 15:57:28 +0900

    14.12 모든 섹션들 구현하기💡 (노트편)

--- 12-toy-project/dist/app.js
+++ 12-toy-project/dist/app.js
@@ -1,3 +1,5 @@
+import { TodoComponent } from "./components/page/item/todo.js";
+import { NoteComponent } from "./components/page/item/note.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { PageComponent } from "./components/page/page.js";
 class App {
@@ -6,6 +8,10 @@ class App {
         this.page.attachTo(appRoot);
         const image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
         image.attachTo(appRoot, "beforeend");
+        const note = new NoteComponent("Note Title", "Note Body");
+        note.attachTo(appRoot, "beforeend");
+        const todo = new TodoComponent("Todo Title", "Todo Item");
+        todo.attachTo(appRoot, "beforeend");
     }
 }
 new App(document.querySelector(".document"));
